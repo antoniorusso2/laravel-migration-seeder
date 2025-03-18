2a14b1a06bcf46e6d9934e827fff8f8e27fb42f9
antoniorusso2 <russoantonio303@outlook.it>
2025-03-18 15:35:19 +0100

    first commit: add project template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Treni oggi</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <h1 class="text-center text-5xl">Treni odierni</h1>
+
+        <section class="train__section"></section>ection
+
+    </div>
+
+</body>
+
+</html>
